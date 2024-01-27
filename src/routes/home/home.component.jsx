ac51f02f61444b45d0f0a2599df07bf4a40865dc
sumedh16 <69754343+sumedh16@users.noreply.github.com>
2024-01-28 00:19:45 +0530

    Added router and navbar component

---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import Directory from '../../components/directory-item/directory-item'
+class Home extends Component{
+  
+  render(){
+    const items=[
+      {
+        "id": 1,
+        "title": "Hats",
+        "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+      },
+      {
+        "id": 2,
+        "title": "Jackets",
+        "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+      },
+      {
+        "id": 3,
+        "title": "Sneakers",
+        "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+      },
+      {
+        "id": 4,
+        "title": "Women",
+        "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+      },
+      {
+        "id": 5,
+        "title": "Men",
+        "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+      }
+    ]
+    return(
+      <Directory categoryList={items}/>
+    );
+  }
+}
+export default Home;
